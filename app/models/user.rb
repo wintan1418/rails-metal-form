47a21135638ac5d5d133b408dced4c5b87ee8462
Ijay Abby <inyangasi4@gmail.com>
2021-02-03 15:38:01 +0300

    modifies validations in the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-    validates :username, presence: true
-    validates :email, presence: true
-    validates :password,presence:true
+    before_save { self.email.downcase! }  
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :username, presence: true, uniqueness:true
+    validates :email, presence: true, uniqueness: {case_sensitive: false}, 
+    length: {maximum: 50}, format: { with: VALID_EMAIL_REGEX }
+
+    validates :password,presence:true, length: {minimum: 5}
 end
