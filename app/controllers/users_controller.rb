8d2b403475cfbf3cd16c2a3db01e46136644f14b
Ijay Abby <inyangasi4@gmail.com>
2021-02-04 12:27:25 +0300

    create hash for user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,16 @@ class UsersController < ApplicationController
     end
 
     def create
-        
+        user_params = {"username"=>"username", "email"=>"email", "password"=>"[FILTERED]"}
+
+        @user = User.new(user_params)
+
+        if @user.save
+            redirect_to_new_user_path
+        else
+            render :new
+
+        end
     end
 
 end
