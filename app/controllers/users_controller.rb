c14a6b0cb68e89fd4d34fb01ff082af53198e3ed
Ijay Abby <inyangasi4@gmail.com>
2021-02-04 17:54:23 +0300

    modify edit.html.erb file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,38 +1,36 @@
 class UsersController < ApplicationController
     def new
-         @user = User.new
+      @user = User.new
     end
-
+  
     def create
-        user_params = {:username=>"username", :email=>"email", :password=>"[FILTERED]"}
-        @user = User.new(user_params)
-
-
-    if @user.save
-      redirect_to new_user_path
-    else
-      render :new
+      # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+      @user = User.new(user_params)
+  
+      if @user.save
+        redirect_to new_user_path
+      else
+        render :new
+      end
     end
-    
-    end
-
+  
     def edit
-        @user = User.find(params[:id])
+      @user = User.find(params[:id])
     end
-
+  
     def update
-        @user = User.find(params[:id])
-        if @user.update(user_params)
-            redirect_to  edit_user_path
-        else
-            render :edit
-        end
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        redirect_to edit_user_path
+      else
+        render :edit
+      end
     end
-
+  
     private
-
+  
     def user_params
-        params.require(:user).permit(:username, :email, :password)
+      params.require(:user).permit(:username, :email, :password)
     end
-
 end
+
