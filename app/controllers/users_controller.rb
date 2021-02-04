02663266cd1527e7ee6c190e941736a44e4efbe8
Ijay Abby <inyangasi4@gmail.com>
2021-02-04 13:19:30 +0300

    add private method that uses require and permit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,38 @@
 class UsersController < ApplicationController
     def new
-
+         @user = User.new
     end
 
     def create
-        user_params = {:username=>"username", :email=>"email", :password=>"[FILTERED]"}
+        whitelisted_params = {:username=>"username", :email=>"email", :password=>"[FILTERED]"} 
 
-        @user = User.new(user_params)
+        @user = User.new(whitelisted_params)
 
         if @user.save
             redirect_to_new_user_path
         else
             render :new
-
-
         end
+    
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(whitelisted_params)
+            redirect_to  '/'
+        else
+            render 'edit'
+        end
+    end
+
+    private
+
+    def whitelisted_params
+        params.require(:user).permit(:username, :email, :password)
     end
 
 end
