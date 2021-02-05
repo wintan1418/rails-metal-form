c53075cf445a958ad8ce0850cc52588af3a16542
oluwadare <wintan1418@gmail.com>
2021-02-05 08:54:56 +0100

    add linters and run rubocop check on codes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-    before_save { self.email.downcase! }  
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :username, presence: true, uniqueness:true
-    validates :email, presence: true, uniqueness: {case_sensitive: false}, 
-    length: {maximum: 50}, format: { with: VALID_EMAIL_REGEX }
+  before_save { email.downcase! }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false },
+                    length: { maximum: 50 }, format: { with: VALID_EMAIL_REGEX }
 
-    validates :password,presence:true, length: {minimum: 5}
+  validates :password, presence: true, length: { minimum: 5 }
 end
